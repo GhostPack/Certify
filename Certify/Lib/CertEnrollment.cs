e7e56ff9b00182462d41b0d990ad6868e57e755e
bytewreck <11381503+bytewreck@users.noreply.github.com>
2025-08-19 10:40:35 +0200

    Fixed private key conversion issue on Windows Server 2012

--- Certify/Lib/CertEnrollment.cs
+++ Certify/Lib/CertEnrollment.cs
@@ -230,16 +230,13 @@ namespace Certify
             var csp_info = new CCspInformations();
             csp_info.AddAvailableCsps();
 
-            var private_key = new CX509PrivateKey
-            {
-                Length = key_size,
-                KeySpec = X509KeySpec.XCN_AT_SIGNATURE,
-                KeyUsage = X509PrivateKeyUsageFlags.XCN_NCRYPT_ALLOW_ALL_USAGES,
-                MachineContext = machine_context,
-                ExportPolicy = X509PrivateKeyExportFlags.XCN_NCRYPT_ALLOW_EXPORT_FLAG,
-                CspInformations = csp_info
-            };
-
+            var private_key = (IX509PrivateKey)Activator.CreateInstance(Type.GetTypeFromProgID("X509Enrollment.CX509PrivateKey"));
+            private_key.Length = key_size;
+            private_key.KeySpec = X509KeySpec.XCN_AT_SIGNATURE;
+            private_key.KeyUsage = X509PrivateKeyUsageFlags.XCN_NCRYPT_ALLOW_ALL_USAGES;
+            private_key.MachineContext = machine_context;
+            private_key.ExportPolicy = X509PrivateKeyExportFlags.XCN_NCRYPT_ALLOW_EXPORT_FLAG;
+            private_key.CspInformations = csp_info;
             private_key.Create();
 
             return private_key;
